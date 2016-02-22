6845ba05810c1496962480a186c3bb61927e6089
djeeg <aceclaw@hotmail.com>
2016-02-22 21:00:13 +0000

    babel presets for treeshaking

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' }
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader', query: { presets: ["react", "es2015-webpack"] } }
     ]
   },
 
